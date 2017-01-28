fb5aad365453d5fbd89c631d8e359bcce954a70a
samuelegastaldo <samuelegastaldo@Lab06_05.itseinaudi.local>
2017-01-28 10:34:07 +0100

    istruzioni, creazione socket e ricezione collegamento

--- src/gastaldo/Client.java
+++ src/gastaldo/Client.java
@@ -2,6 +2,14 @@ package gastaldo;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.List;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
 
 public class Client {
 
@@ -22,12 +30,15 @@ public class Client {
 
 	/**
 	 * Open the window.
+	 * @throws IOException 
+	 * @throws UnknownHostException 
 	 */
-	public void open() {
+	public void open() throws UnknownHostException, IOException {
 		Display display = Display.getDefault();
 		createContents();
 		shell.open();
 		shell.layout();
+		avvia();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
@@ -42,7 +53,17 @@ public class Client {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
+		
+		List list = new List(shell, SWT.BORDER);
+		list.setBounds(10, 10, 162, 202);
+		
+		Button btnLista = new Button(shell, SWT.NONE);
+		btnLista.setBounds(214, 10, 75, 25);
+		btnLista.setText("Lista");
 
 	}
-
+	
+	public void avvia() throws UnknownHostException, IOException{
+		Socket s=new Socket("localhost", 9999);
+	}
 }
--- src/gastaldo/Server.java
+++ src/gastaldo/Server.java
@@ -1,5 +1,14 @@
 package gastaldo;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss=new ServerSocket(9999);
+		while(true){
+			Socket s=ss.accept();
+		}
+	}
 }
